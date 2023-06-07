80cc1f1a78ecbd905fd85890a9f4bffd0f8fe13d
IsaacCB <127397000+Isaac-Caballero@users.noreply.github.com>
2023-06-07 09:55:51 +0200

    Add files via upload

---
+++ isaacCB/src/test/java/com/prueba/curso/jenkins/isaacCB/IsaacCbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.curso.jenkins.isaacCB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsaacCbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
