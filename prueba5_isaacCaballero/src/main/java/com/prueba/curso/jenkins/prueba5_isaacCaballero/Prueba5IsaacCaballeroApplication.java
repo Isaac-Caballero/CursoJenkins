b457e08a0539a62355e6a58585a8bd0a96d0e4bc
IsaacCB <127397000+Isaac-Caballero@users.noreply.github.com>
2023-06-07 13:05:24 +0200

    Add files via upload

---
+++ prueba5_isaacCaballero/src/main/java/com/prueba/curso/jenkins/prueba5_isaacCaballero/Prueba5IsaacCaballeroApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.curso.jenkins.prueba5_isaacCaballero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prueba5IsaacCaballeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prueba5IsaacCaballeroApplication.class, args);
+	}
+
+}
---
+++ prueba5_isaacCaballero/src/test/java/com/prueba/curso/jenkins/prueba5_isaacCaballero/Prueba5IsaacCaballeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.curso.jenkins.prueba5_isaacCaballero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prueba5IsaacCaballeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
